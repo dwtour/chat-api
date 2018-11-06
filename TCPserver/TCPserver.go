d468a698a6c57daf1c818088b6423c3a93de2af7
DonRIn <mykeyofheart@gmail.com>
2018-11-06 21:28:30 +0300

    added func for sending messages to all users

--- TCPserver/TCPserver.go
+++ TCPserver/TCPserver.go
@@ -5,7 +5,15 @@ import (
 	"io"
 	"log"
 	"net"
+	"sync"
 )
+
+type SafeConnect struct{
+	list map[string]net.Conn
+	mux sync.Mutex
+}
+
+var listConnect = SafeConnect{list:make(map[string]net.Conn)}
 
 func Listen(){
 	listener, err := net.Listen("tcp", ":8080")
@@ -19,11 +27,14 @@ func Listen(){
 		if err != nil {
 			log.Fatal("Error accepting", err.Error())
 		}
+
+		listConnect.list[conn.RemoteAddr().String()] = conn
 		go handleRequest(conn)
 	}
 }
 //testing connection
 func handleRequest(conn net.Conn) {
+	chMessage := make(chan []byte, 100)
 	buf := make([]byte, 1024)
 	for {
 		n, err := conn.Read(buf)
@@ -33,9 +44,24 @@ func handleRequest(conn net.Conn) {
 			}
 			break
 		}
-		fmt.Println(string(buf[:n]) + "(Message received).")
+		chMessage <- buf[:n]
+
+		go sendMessage(chMessage, conn)
+		//fmt.Println(string(buf[:n]) + "(Message received).")
 	}
+	delete(listConnect.list, conn.RemoteAddr().String())
 	fmt.Println("All messages received.")
+
 	conn.Close()
 }
 
+func sendMessage(ch chan []byte, conn net.Conn) {
+	listConnect.mux.Lock()
+	mes := <- ch
+	for _, v := range listConnect.list {
+		if (v != conn) {
+			v.Write(mes)
+		}
+	}
+	listConnect.mux.Unlock()
+}
